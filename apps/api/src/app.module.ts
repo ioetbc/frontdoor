90ba476ce11a3318578358c753757c31678b68cb
ioetbc <ioetbc@gmail.com>
2023-04-13 14:36:20 +0100

    add gql and mongodb

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { ApolloDriver } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      playground: true,
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_CONNECTION_STRING),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

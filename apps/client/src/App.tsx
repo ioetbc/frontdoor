c4f386e95cee5b757d7fbfc2b1a304240c0e25ef
ioetbc <ioetbc@gmail.com>
2023-04-13 10:53:07 +0100

    initial commit

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,40 @@
+import {useEffect, useState} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+  const [greeting, setGreeting] = useState<string>("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.text())
+      .then(setGreeting);
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">{greeting}</p>
+    </div>
+  );
+}
+
+export default App;
